3556d69fea74385ee155f6f2ef212a520e55e6d8
QuangAnh845 <quanganh845@gmail.com>
2026-06-06 18:34:37 +0700

    chen audio

--- my-backend/src/config/env.ts
+++ my-backend/src/config/env.ts
@@ -3,6 +3,6 @@ import * as dotenv from 'dotenv';
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://admin:admin123@cluster0.sro5fph.mongodb.net/vietvibe_db?appName=Cluster0';
+export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/vietvibe_db';
 export const PORT = Number(process.env.PORT || 3001);
 export const JWT_SECRET = process.env.JWT_SECRET || 'default_secret_key';
